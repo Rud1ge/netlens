481b26114f6c347b870658027e6734e938b73b1e
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-01-21 13:01:26 +0700

    Added parsing of ARP packets

--- protocols/arp.py
+++ protocols/arp.py
@@ -1,25 +1,52 @@
-from rich.console import Console
-from rich.table import Table
-from scapy.layers.l2 import ARP, Ether
-from scapy.utils import mac2str
-import struct
+from scapy.compat import raw
+from scapy.layers.l2 import ARP
+
+arp_hwtype = {
+    1: "Ethernet",
+    6: "IEEE 802",
+    15: "Frame Relay",
+    16: "ATM",
+    19: "ATM",
+    20: "Serial Line",
+    24: "IEEE 1394",
+    27: "EUI-64",
+    32: "InfiniBand",
+}
+
+arp_ptype = {0x0800: "IPv4", 0x86DD: "IPv6"}
+
+arp_op = {
+    1: "request",
+    2: "reply",
+    3: "RARP request",
+    4: "RARP reply",
+    8: "InARP request",
+    9: "InARP reply",
+}
 
 
-def arp(packet):
-    if not packet.haslayer(ARP):
-        return
+def arp(packet, table):
+    if ARP not in packet:
+        return False
 
-    table = Table(title=f"Address Resolution Protocol", title_justify="left")
-    table.add_column("Layer")
-    table.add_column("Field")
-    table.add_column("Weight")
-    table.add_column("Value")
+    arp_layer = packet[ARP]
 
-    eth = packet.getlayer(Ether)
-    if eth:
-        table.add_row("Ethernet", "Source MAC Address", f"{len(mac2str(eth.src))} bytes", f"{eth.src}")
-        table.add_row("Ethernet", "Destination MAC Address", f"{len(mac2str(eth.dst))} bytes", f"{eth.dst}")
-        table.add_row("Ethernet", "EtherType", f"{len(struct.pack('!H', int(eth.type)))} bytes", f"{hex(eth.type)}")
+    hwtype = int(arp_layer.hwtype)
+    ptype = int(arp_layer.ptype)
+    op = int(arp_layer.op)
 
-    console = Console()
-    console.print(table)
+    table.add_row("ARP", "Hardware type", "16", f"{hwtype} ({arp_hwtype[hwtype]})")
+    table.add_row("ARP", "Protocol type", "16", f"{hex(ptype)} ({arp_ptype[ptype]})")
+    table.add_row("ARP", "Hardware size", "8", str(int(arp_layer.hwlen)))
+    table.add_row("ARP", "Protocol size", "8", str(int(arp_layer.plen)))
+    table.add_row("ARP", "Operation", "16", f"{op} ({arp_op[op]})")
+    table.add_row("ARP", "Sender MAC", str(int(arp_layer.hwlen) * 8), str(arp_layer.hwsrc))
+    table.add_row("ARP", "Sender IP", str(int(arp_layer.plen) * 8), str(arp_layer.psrc))
+    table.add_row("ARP", "Target MAC", str(int(arp_layer.hwlen) * 8), str(arp_layer.hwdst))
+    table.add_row("ARP", "Target IP", str(int(arp_layer.plen) * 8), str(arp_layer.pdst))
+
+    actual_bytes = len(raw(arp_layer)) - len(raw(arp_layer.payload))
+    table.caption = f"ARP PDU length: {actual_bytes * 8} bits ({actual_bytes} bytes)"
+    table.caption_justify = "left"
+
+    return True
