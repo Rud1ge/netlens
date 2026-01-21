ef6e539c7a10999d4e2f42f6a43ef0ee0920f4ef
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-01-21 10:45:35 +0700

    Counting the length of Ethernet headers is supported

--- protocols/arp.py
+++ protocols/arp.py
@@ -1,6 +1,8 @@
 from rich.console import Console
 from rich.table import Table
 from scapy.layers.l2 import ARP, Ether
+from scapy.utils import mac2str
+import struct
 
 
 def arp(packet):
@@ -15,9 +17,9 @@ def arp(packet):
 
     eth = packet.getlayer(Ether)
     if eth:
-        table.add_row("Ethernet", "Source MAC Address", {len(bytes(eth.src))}, f"{eth.src}")
-        table.add_row("Ethernet", "Destination MAC Address", "6 bytes", f"{eth.dst}")
-        table.add_row("Ethernet", "EtherType", "2 bytes", f"{hex(eth.type)}")
+        table.add_row("Ethernet", "Source MAC Address", f"{len(mac2str(eth.src))} bytes", f"{eth.src}")
+        table.add_row("Ethernet", "Destination MAC Address", f"{len(mac2str(eth.dst))} bytes", f"{eth.dst}")
+        table.add_row("Ethernet", "EtherType", f"{len(struct.pack('!H', int(eth.type)))} bytes", f"{hex(eth.type)}")
 
     console = Console()
     console.print(table)
