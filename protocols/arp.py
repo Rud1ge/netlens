79f568133c035ced719f26444e30f9eafe4d5f03
Руди Герман Геннадьевич <german.rudi@eltex.loc>
2026-01-21 10:33:26 +0700

    Rich integration has been added to display information in a tabular format

--- protocols/arp.py
+++ protocols/arp.py
@@ -1,5 +1,5 @@
-import sys
-
+from rich.console import Console
+from rich.table import Table
 from scapy.layers.l2 import ARP, Ether
 
 
@@ -7,8 +7,17 @@ def arp(packet):
     if not packet.haslayer(ARP):
         return
 
+    table = Table(title=f"Address Resolution Protocol", title_justify="left")
+    table.add_column("Layer")
+    table.add_column("Field")
+    table.add_column("Weight")
+    table.add_column("Value")
+
     eth = packet.getlayer(Ether)
     if eth:
-        sys.stdout.write(f"ARP: ether {eth.src} -> {eth.dst} type={hex(eth.type)}\n")
-    else:
-        sys.stdout.write("ARP: (no Ether layer)\n")
+        table.add_row("Ethernet", "Source MAC Address", {len(bytes(eth.src))}, f"{eth.src}")
+        table.add_row("Ethernet", "Destination MAC Address", "6 bytes", f"{eth.dst}")
+        table.add_row("Ethernet", "EtherType", "2 bytes", f"{hex(eth.type)}")
+
+    console = Console()
+    console.print(table)
